8c2a07cddc677b294308abbb3848298beb464d42
Aman Sharma <76823502+arcAman07@users.noreply.github.com>
2023-04-25 00:20:14 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='ocr_dict_lookup',
     version='0.1.0',
     description='Extracts highlighted words from images and looks up their meanings in an online dictionary.Basically a tool to help you in your GRE preperation',
-    author='Aman Sharma',
+    author='Aman Sharma @arcAman07',
     author_email='amananytime07@gmail.com',
     packages=find_packages(),
     install_requires=[
