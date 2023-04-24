057b5c60027a9b3dc6a4ad2c0b853ef4041c320e
arcAman07 <amananytime07@gmail.com>
2023-04-25 03:39:09 +0530

    unit tests added

--- app.py
+++
@@ -1,8 +0,0 @@
-from ocr_dict_lookup import extract_highlighted_words, get_word_meanings, write_word_meanings
-
-# Compare this snippet from ocr_dict_lookup/ocr_dict_lookup.py:
-
-image_path = "/Users/deepaksharma/Documents/Languages/Python/ocr_dict_lookup/images/IMG_0025.PNG"
-
-words = extract_highlighted_words(image_path)
-print(words)
